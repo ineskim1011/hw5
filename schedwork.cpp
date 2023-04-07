d98f134c12ae81b44f985a85611e4ed168fb2242
Ines Kim <iskim@usc.edu>
2023-04-07 04:48:33 +0000

    finished with HW5

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,8 +19,16 @@ using namespace std;
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
-
+// Helper for recursive calling 
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		vector<vector<size_t>>& numShifts,
+		size_t currDay,
+		size_t currWorker
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -31,14 +39,93 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
+    if (avail.size() == 0) {
+      return false;
     }
     sched.clear();
-    // Add your code below
 
+		// Initialize the Daily Schedule return value (2D array of Worker IDs)
+		for (size_t i = 0; i < avail.size(); i++) {
+			sched.push_back(vector<Worker_T>());
+		}
 
+		// Initialize a 2D array to count running number of shifts for each worker 
+		vector<vector<size_t>> numShifts;
+		for (size_t i = 0; i < avail[0].size(); i++) {
+			numShifts.push_back(vector<size_t>());
+			numShifts[i].push_back(0);
+		}
+		
+		// Call helper function for first day, first shift which will recurse for all days 
+		// Returns true if schedule is possible with given parameters, false otherwise
+		if (scheduleHelper(avail, dailyNeed, maxShifts, sched, numShifts, 0, 0)) { return true; }
 
+		return false;
+}
 
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		vector<vector<size_t>>& numShifts,
+		size_t currDay,
+		size_t currWorkers
+)
+{	
+		// Base Case: if current day has been incremented to a non-existent day (i.e. past the alloted days)
+		// that means that the schedule is possible 
+		if (currDay == avail.size()) {
+			return true;
+		}
+		
+		// If number of workers for a day is equal to the amount of workers required per day, 
+		// recursively call for next day
+		if (currWorkers == dailyNeed) {
+			return scheduleHelper(avail, dailyNeed, maxShifts, sched, numShifts, currDay + 1, 0);
+		}
+
+		// Tallies number of workers we are adding to number already working 
+		size_t addedWorkers = currWorkers;
+		// Iterate through workers' availability for the given day 
+		for (size_t i = 0; i < avail[currDay].size(); i++) {
+			// Runs if the worker is not already scheduled for that day 
+			if (find(sched[currDay].begin(), sched[currDay].end(), i) == sched[currDay].end()) {
+				// Runs if the worker is available for that day 
+				if (avail[currDay][i]) {
+					// Runs if the worker has not maxed out their shifts 
+					if (numShifts[i][0] < maxShifts) {
+						// Add this worker to this day, so increment number of added workers 
+						addedWorkers++;
+						// Add this worker to the schedule for this day 
+						sched[currDay].push_back(i);
+						// Increment worker's number of shifts in vector created in main Schedule function 
+						numShifts[i][0]++;
+						// If number of daily workers needed is reached, reset added workers for the 
+						// day to zero and move onto the next day, this one is complete 
+						if (dailyNeed == sched[currDay].size()) {
+							addedWorkers = 0;
+							currDay++;
+						}
+						// Recursive call to Helper for next day 
+						if (scheduleHelper(avail, dailyNeed, maxShifts, sched, numShifts, currDay, addedWorkers)) { return true; }
+						// Backtrack case: if recursive call is false, undo by: 
+						// decrementing number of shifts,
+						numShifts[i][0]--;
+						// going back to the past day if the schedule for the current day has not 
+						// been made yet (i.e. this day wasn't the problem)
+						if (sched[currDay].size() == 0) {
+							currDay--;
+						}
+						// taking the last addition off the scheudle 
+						sched[currDay].pop_back();
+						// starting over (i.e. added workers reset to current workers)
+						addedWorkers = currWorkers;
+					}
+				}
+			}
+		}
+		// return false otherwise 
+		return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -12,8 +12,80 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
+// Checks if created permutation is a real word in the English dictionary 
+bool isAWord(
+		const std::string& in,
+		const std::set<std::string>& dict)
+{
+		if (dict.find(in) != dict.end()) {
+			return true;
+		}
+		return false;
+}
 
+
+// Wordle helper for recursive use
+void wordMaker(
+			std::string& semiWord,
+			std::string floats,
+			const std::set<std::string>& dict,
+			std::set<std::string>& results,
+			std::string& letters,
+			size_t position, 
+			size_t dashRemaining)
+{
+	// Holds original float string for backtracking purposes 
+	std::string floatsHolder = floats;
+	// If no dashes left in word, checks if permutation is a real word
+	// If so, inserts into results 
+	if (position == semiWord.length()) {
+		if (isAWord(semiWord, dict)) {
+			results.insert(semiWord);
+		}
+		return;
+	}
+	// If current position already has a letter, calls helper for next position in word 
+	else if (semiWord[position] != '-') {
+		wordMaker(semiWord, floats, dict, results, letters, position + 1, dashRemaining);
+	}
+	// If the remaining empty characters can only be floats (i.e. number of letters remaining is same number of floats)
+	// iterate ONLY through floats (not the whole alphabet)
+	else if (dashRemaining == floats.size()) {
+		for (size_t i = 0; i < floats.size(); i++){
+			semiWord[position] = floats[i];
+			dashRemaining--; 
+			// Erases current float from string so that future recursive calls on next positions 
+			// will not repeat it 
+			floats.erase(floats.begin() + i);
+			// Recursive call to helper using next position 
+			// Will iterate through floats ONLY due to number of floats being equal to remaining dashes 
+			wordMaker(semiWord, floats, dict, results, letters, position + 1, dashRemaining);
+			// Backtracking: get rid of current float and iterate to next float 
+			dashRemaining++;
+			floats = floatsHolder;
+			semiWord[position] = '-'; 
+		}
+	}
+	// Iterates through alphabet 
+	else {
+		for (size_t i = 0; i < letters.size(); i++) {
+			semiWord[position] = letters[i];
+			dashRemaining -= 1; 
+			// If a floater is used, remove it from float string 
+			for (size_t j = 0;  j < floats.size(); j++){
+				if (floats[j] == letters[i]){
+					floats.erase(floats.begin() + j);
+					break;
+				}
+			}
+			wordMaker(semiWord, floats, dict, results, letters, position + 1, dashRemaining);
+			// Backtracking (same as for floaters)
+			dashRemaining++;
+			semiWord[position] = '-';
+			floats = floatsHolder;
+		}
+	}
+}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +93,26 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+		std::set<std::string> results;
+		std::string startWord = in;
+		std::string floats = floating;
+		// String to iterate through alphabet 
+		std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+		// If word size is zero, return results which is empty 
+		if (in.size() == 0) {
+			return results;
+		}
 
+		// Counts number of dashes in the given string (to use in helper)
+		int dashCount = 0;
+		for (size_t i = 0; i < in.size(); i++) {
+			if (in[i] == '-') {
+				dashCount++;
+			}
+		}
+
+		// Call helper starting at first position of given string (0th index)
+		wordMaker(startWord, floats, dict, results, alphabet, 0, dashCount);
+	
+		return results;
 }
-
-// Define any helper functions here
